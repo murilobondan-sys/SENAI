9b3b64414ca8ee3ff9e43b6de3205b03ec12e66f
murilobondan-sys <murilo_bondan@estudante.sesisenai.org.br>
2026-03-11 19:21:49 +0000

    update

--- atividade python/atividade10.py
+++ atividade python/atividade10.py
@@ -1,21 +1,24 @@
-maior = float()
-menor = float()
 soma = 0 
 acima_15 = 0
-for cont in range(10):
+acima_20 = 0
+alerta = False
+sobrecarga = False
+acima_200 = 0
+for cont in range(8):
     amper = float(input(f"digite o {cont + 1} amper: "))
     soma += amper
-    if cont == 0:
-        maior = amper
-        menor = amper
     if amper > 15:
         acima_15 += 1
-        if amper > maior:
-            maior = amper
-        if amper < menor:
-            menor = amper
-media = soma / 10
-print(f"Amper media: {media}")
-print(f"Amper maior: {maior}")
-print(f"Amper menor: {menor}")    
+    if amper > 20:
+        sobrecarga = True
+        acima_20 += 1
+        if amper > 200:
+            alerta = True
+            acima_200 += 1
+media = soma / 8
+print(f"Media de amperes: {media}") 
 print(f"quantidade de amperes acima de 15: {acima_15} vezes")
+if sobrecarga:
+    print(f"houve sobrecarga no motor {acima_20} vezes")
+    if alerta:
+        print(f"alerta: ultrapassou 200 {acima_200} vezes")
